5f74338b3de8681f5adc03827a3fe7007f446777
Davi27 <67101009+Davi27@users.noreply.github.com>
2026-05-16 17:53:14 +0100

    adapted to the new classes

--- a_maze_ing/a_maze_ing.py
+++ a_maze_ing/a_maze_ing.py
@@ -1,10 +1,29 @@
+import sys
+from classes import Cell, Grid, RecursiveBacktracker
+from errors import config_parser, AnyError
 
-def get_info():
-    with open("config.txt", "r") as f:
-        lines = f.readlines()
-        
-    for line in lines:
-        print(line.strip())
+
+def main() -> None:
+    if len(sys.argv) != 2:
+        print("Usage: python3 a_maze_ing.py config.txt")
+        return
+
+    try:
+        vals = config_parser(sys.argv[1])
+    except AnyError as e:
+        print(e)
+        return
+
+    grid = Grid(vals[1], vals[0])
+    GREEN = "\033[92m"
+    RED = "\033[91m"
+    RESET = "\033[0m"
+    grid[vals[2]].body = f"{GREEN} E {RESET}" 
+    grid[vals[3]].body = f"{RED} X {RESET}"
+
+    RecursiveBacktracker.on(grid)
+    print(grid)
+
 
 if __name__ == "__main__":
-    get_info()
+    main()
